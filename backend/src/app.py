30726e31847eebeffd66d4239ed5dc330d59aa89
Sidir13 <saidi.idir42@gmail.com>
2025-10-02 16:12:03 +0200

    modified port api

--- backend/src/app.py
+++ backend/src/app.py
@@ -146,5 +146,5 @@ def translate():
 # Lancement serveur
 # ==============================
 if __name__ == "__main__":
-    # S’assure que app est bien défini avant exécution
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
